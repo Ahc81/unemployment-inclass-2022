d3a2e1988b580bec91220afc211419d9be4e48b3
Ahc81 <115958440+Ahc81@users.noreply.github.com>
2022-10-26 18:01:59 -0400

    Ready to upload

---
+++ app/unemployment.py
@@ -0,0 +1,26 @@
+'''import requests
+import json
+from pprint import ppgrint
+
+
+#working in app/unemployment_report.py file
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
+'''
+
+import os
+import json
+from pprint import pprint
+
+import requests
+
+API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
+
+response = requests.get(request_url)
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response))
+pprint(parsed_response)
