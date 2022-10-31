10ec8ba836aee4d0311600934f16daa42c15eb52
Ahc81 <115958440+Ahc81@users.noreply.github.com>
2022-10-31 10:37:43 -0400

    Now with Challenge C Included

--- app/unemployment.py
+++ app/unemployment.py
@@ -35,6 +35,9 @@ print("Latest unemployment rate:")
 #print(data[0])
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
 
+
+#Challenge B 
+# Average unemployment rate for the current year with the amount of months in the year
 this_year = [d for d in data if "2022-" in d["date"]]
 
 rates_this_year = [float(d["value"]) for d in this_year]
@@ -43,5 +46,13 @@ rates_this_year = [float(d["value"]) for d in this_year]
 print("-------------------------")
 print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
 print("NO MONTHS:", len(this_year))
-breakpoint()
 
+# Challenge C
+#Plotting unemployment line
+
+
+dates = [d["date"] for d in data]
+rates = [float(d["value"]) for d in data]
+
+fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+fig.show()
